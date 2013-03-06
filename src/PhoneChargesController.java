d909e364b5c4686d7d2600a856b7148ff6f589ba
Daniel Hodgson <daniel.hodgson@codeprogrammers.net>
2013-03-06 06:29:52 -0800

    Added listener class for Calculate button

--- src/PhoneChargesController.java
+++ src/PhoneChargesController.java
@@ -34,4 +34,36 @@ public class PhoneChargesController
         }
     }
 
+    /**
+     * Listener class for the calculate button: 
+     * Validates input for durationTextField (displays error if input is invalid), 
+     * calls on Model to set duration and calculate charges, 
+     * calls on View to display call information
+     *
+     * @author Daniel Hodgson (daniel.hodgson@codeprogrammers.net)
+     * @version 1.0
+     */
+    class CalcButtonListener implements  ActionListener
+    {
+        @Override
+        public void actionPerformed(ActionEvent e)
+        {
+            try
+            {
+                String input = view.getUserInput();
+                int duration = Integer.parseInt(input);
+
+                // Duration and rate have to be set before getCharges() will return the correct value
+                model.setDuration(duration);
+                double charges = model.getCharges();
+                double rate = model.getRate();
+
+                view.showCharges(duration, rate, charges);
+            }
+            catch (NumberFormatException nfe)
+            {
+                view.showError("Input error", "Minutes not valid");
+            }
+        }
+    }
 }
